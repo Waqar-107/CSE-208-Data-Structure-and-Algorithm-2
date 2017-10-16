5017a94412cad732b3cb0cce720f68e0ae95eb32
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2017-10-16 20:35:13 +0700

    Update 1505107_dp.cpp

--- week-5/1505107_dp.cpp
+++ week-5/1505107_dp.cpp
@@ -14,66 +14,88 @@ using namespace std;
 
 int main()
 {
-    //freopen("in.txt","r",stdin);
+	//freopen("in.txt", "r", stdin);
 
-    int i, j, k;
-    int n, x,v;
+	int i, j, k;
+	int n, x, v;
 
-    scanf("%d%d", &v, &n);
+	scanf("%d%d", &v, &n);
 
-    int coins[n];
-    int q[n];
+	int *coins=new int[n];
+	int *q=new int[n];
 
-    for(i=0; i<n; i++)
-        scanf("%d",&coins[i]);
+	for (i = 0; i<n; i++)
+		scanf("%d", &coins[i]);
 
-    for(i=0; i<n; i++)
-        scanf("%d",&q[i]);
+	for (i = 0; i<n; i++)
+		scanf("%d", &q[i]);
 
-    int tab[v+1];
-    int used[n][v+1];
+	int *tab=new int[v + 1];
 
-    tab[0]=0;
-    for(i=1; i<=v; i++)
-        tab[i]=inf;
+	int **used = new int*[n];
+	for (i = 0; i < n; i++)
+	{
+		used[i] = new int[v + 1];
+	}
 
-    memset(used,0,sizeof(used));
+	tab[0] = 0;
+	for (i = 1; i <= v; i++)
+		tab[i] = inf;
 
-    for(i=1; i<=v; i++)
-    {
-        for(j=0; j<n; j++)
-        {
-            x=tab[i-coins[j]];
-            if(i>=coins[j])
-            {
-                if(x+1<tab[i] && x!=inf && used[j][i-coins[j]]+1<=q[j])
-                {
-                    tab[i]=x+1;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= v; j++)
+			used[i][j] = 0;
+	}
 
-                    for(k=0; k<n; k++)
-                        used[k][i]=used[k][i-coins[j]];
+	//----------------------------------------------------------------
+	for (i = 1; i <= v; i++)
+	{
+		for (j = 0; j<n; j++)
+		{
+			if (i - coins[j] < 0)
+				continue;
 
-                    used[j][i]=used[j][i-coins[j]]+1;
-                }
-            }
-        }
-    }
+			x = tab[i - coins[j]];
+			if (i >= coins[j])
+			{
+				if (x + 1<tab[i] && x != inf && used[j][i - coins[j]] + 1 <= q[j])
+				{
+					tab[i] = x + 1;
 
-    if(tab[v]!=inf)
-    {
-        printf("%d\n",tab[v]);
+					for (k = 0; k<n; k++)
+						used[k][i] = used[k][i - coins[j]];
 
-        printf("notes used:\n");
-        for(i=0; i<n; i++)
-        {
-            if(used[i][v]!=0)
-                printf("%d * %d\n",coins[i],used[i][v]);
-        }
-    }
+					used[j][i] = used[j][i - coins[j]] + 1;
+				}
+			}
+		}
+	}
+	//----------------------------------------------------------------
 
-    else
-        printf("Impossible\n");
+	if (tab[v] != inf)
+	{
+		printf("%d\n", tab[v]);
+
+		printf("notes used:\n");
+		for (i = 0; i<n; i++)
+		{
+			if (used[i][v] != 0)
+				printf("%d * %d\n", coins[i], used[i][v]);
+		}
+	}
+
+	else
+		printf("Impossible\n");
+
+	for (i = 0; i < n; i++)
+		delete[] used[i];
+
+	delete[] used;
+	delete[] q;
+	delete[] coins;
+	delete[] tab;
 
 
-    return 0;
+	return 0;
 }
